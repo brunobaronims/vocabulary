c84c65e1a58b927d930b78d1933b309a8207da6e
brunobaronims <brunobaronims@gmail.com>
2026-01-05 10:06:38 -0300

    add README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vocabulary
+
+Aplicação que permite praticar vocabulário em inglês, com gerenciamento de sessão e acompanhamento de progresso.
+
+## Como rodar
+
+Antes de rodar qualquer serviço, renomeie cada `.env.example` para `.env`. Os `.env` em `back` e `front` são necessários apenas para rodar os apps localmente, para fazer mudanças no codigo de maneira mais fácil.
+
+### Local 
+
+- Inicie uma instância do Postgres de sua maneira preferida (`docker compose up postgres -d`).
+
+## Backend
+- `npm run start:dev`
+
+## Frontend
+- `npm run dev`
+
+### Container 
+
+- Use `docker-compose up -d` na raiz do projeto para subir todos os serviços em container.
--- front/src/Login.tsx
+++ front/src/Login.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import logo from './assets/logo-edify-Preto.svg';
 import { gql } from '@apollo/client';
 import { useMutation } from '@apollo/client/react';
 import { toast } from 'sonner';
@@ -90,8 +89,7 @@ function Login() {
   };
 
   return (
-    <main className="w-screen h-screen flex flex-col items-center pt-10">
-      <img src={logo} alt="Edify" className="w-40 h-auto sm:mb-10" />
+    <main className="w-screen h-screen flex items-center pt-10">
       <div className="flex items-center justify-center h-full w-full">
         <div className="flex flex-col items-center h-full w-full sm:w-120 sm:h-fit py-6 md:w-160 shadow-lg rounded-md sm:border border-stone-400/50">
           <div className="h-full w-full px-6 flex flex-col justify-center items-center">
